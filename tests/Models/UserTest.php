230c140c9e25a7e8d496a285e8d0f7e34819266c
Yash <yashch@cybage.com>
2021-03-17 14:24:53 +0530

    Assignment till 17

--- tests/Models/UserTest.php
+++ tests/Models/UserTest.php
@@ -10,4 +10,53 @@ use Illuminate\Http\Response;
 
 class UserTests extends TestCase
 {
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     * Testing if create event is triggered
+     */
+    public function testCreate()
+    {
+        $user = factory(User::class)->create();
+        $activity = Activities::latest()->first();
+        $this->assertEquals($user->id, $activity->resource_id);
+        $this->assertEquals($user->getTable(), $activity->table_name);
+    }
+
+    /**
+     * @test
+     * Testing if update event is triggered
+     */
+    public function testUpdate()
+    {
+        $user = factory(User::class)->create();
+
+        $updatedUser = [
+            'first_name' => $this->faker->firstName,
+            'last_name'  => $this->faker->lastName,
+        ];
+        $user->first_name = $updatedUser['first_name'];
+        $user->last_name = $updatedUser['last_name'];
+
+        $activity = Activities::latest()->first();
+        $this->assertEquals($user->id, $activity->resource_id);
+        $this->assertEquals($user->getTable(), $activity->table_name);
+    }
+
+    /**
+     * @test
+     * Testing if delete event is triggered
+     */
+    public function testDelete()
+    {
+        $user = factory(User::class)->create();
+        $user->delete();
+        $userArray = $user->getAttributes();
+
+        $activity = Activities::latest()->first();
+        $this->assertSoftDeleted('users', $userArray);
+        $this->assertEquals($user->id, $activity->resource_id);
+        $this->assertEquals($user->getTable(), $activity->table_name);
+    }
 }
